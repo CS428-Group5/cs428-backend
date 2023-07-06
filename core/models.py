18911b4528284cec6a9b8190776c37a948255832
hoai1107 <khaihoai1107@gmail.com>
2023-07-06 21:11:05 +0700

    Create the base User model

---
+++ core/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    class Gender(models.IntegerChoices):
+        FEMALE = 0, "Female"
+        MALE = 1, "Male"
+
+    id = models.BigAutoField(primary_key=True)
+    avatar = models.ImageField(upload_to="avatars/", null=True, blank=True)
+    gender = models.IntegerField(choices=Gender.choices, null=True, blank=True)
+    current_title = models.CharField(max_length=255, null=True, blank=True)
+    about_me = models.TextField(null=True, blank=True)
