60487aed9323f1cfe37a3b337f40d51c5a8726d9
quoc <quocnd@inter-k.com>
2023-07-20 18:21:56 +0700

    script to deploy

---
+++ local_mirror_script.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# This script is workable for only Quok's machine
+
+# git checkout main
+git config credential.helper gcloud.sh
+git status
+if ! git remote | grep -q 'google'; then
+    git remote add google https://source.developers.google.com/p/ecomercebackend-393408/r/cs428-backend
+else
+    echo "The google git remote is already existed"
+fi
+git push --set-upstream google quocnd/deploy_backend_gcp
