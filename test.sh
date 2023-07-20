6a89d1521d04d12a930e78b9bf2c1ae8f7a5bb50
quoc <quocnd@inter-k.com>
2023-07-21 03:07:05 +0700

    tune the script

--- test.sh
+++ test.sh
@@ -1,12 +1,12 @@
-echo "if [ ! -d $GIT_FOLDER ]; then
+gcloud compute ssh instance-1 -- command "echo 'if [ ! -d cs428-backend ]; then
         git clone git@github.com:CS428-Group5/cs428-backend.git
-        cd $GIT_FOLDER
+        cd cs428-backend
         python3 -m venv .env
         source .env/bin/activate
         pip install -r requirements.txt
         python3 manage.py migrate
         python3 manage.py runserver
     else
-        cd $GIT_FOLDER
+        cd cs428-backend
         git pull origin master
-    fi" > init_script.sh
+    fi' > init_script.sh"
