09ddee8e54892ab08280dc801641e072489753b9
quoc <quocnd@inter-k.com>
2023-07-21 17:13:44 +0700

    Finishing the task

--- remote_startup.sh
+++ remote_startup.sh
@@ -8,11 +8,10 @@ sudo gcloud auth print-access-token | sudo docker login -u oauth2accesstoken \
 sudo gcloud auth configure-docker https://us-central1-a-docker.pkg.dev
 sudo docker pull $IMAGE
 
-CONTAINER_ID=$(sudo docker ps -qf "ancestor=$IMAGE")
+CONTAINER_ID=$(sudo docker ps -qf "name=backend")
 if [ ! -z "$CONTAINER_ID" ]; then
   sudo docker stop $CONTAINER_ID
-  sudo docker rm $CONTAINER_ID
+  sudo docker rm -v $CONTAINER_ID
 fi
 
-sudo docker run -d -p 8000:8000 $IMAGE
-
+sudo docker run -d -p 8000:8000 --name backend $IMAGE
