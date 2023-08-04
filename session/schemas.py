74192d9c18da6092734a352887f5d41a401522f4
quoc <quocnd@inter-k.com>
2023-08-04 20:24:09 +0700

    fix the schema of the BookedSessionOut

--- session/schemas.py
+++ session/schemas.py
@@ -1,13 +1,22 @@
 from .models import BookedSession, MentorSession
 from ninja import ModelSchema, Schema, Field
-from datetime import time, date
+from pydantic import root_validator
+from datetime import time, date, datetime, timedelta
 
 
 class BookedSessionOutSchema(ModelSchema):
     mentee_id: int = Field(alias="mentee.id")
+    mentor_id: int = Field(alias="mentor_session.mentor.id")
+    session_date: date  = Field(...,alias="mentor_session.session_date")
+    session_time: time = Field(...,alias="mentor_session.session_time")
+    mentor_first_name: str = Field(...,alias="mentor_session.mentor.user.first_name")
+    mentor_last_name: str = Field(...,alias="mentor_session.mentor.user.last_name")
+    mentee_first_name: str = Field(...,alias="mentee.user.first_name")
+    mentee_last_name: str = Field(...,alias="mentee.user.last_name")
+
     class Config:
         model = BookedSession
-        model_exclude = ["mentee"]
+        model_exclude = ["mentee", "mentor_session"]
 
 class MentorSessionInSchema(Schema):
     session_time: time
