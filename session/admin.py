a61df54771380ae6b71907189e09a561d7136969
quoc <quocnd@inter-k.com>
2023-08-04 21:03:42 +0700

    register to admin

--- session/admin.py
+++ session/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import BookedSession, MentorSession
 
 # Register your models here.
+admin.site.register(BookedSession)
+admin.site.register(MentorSession)
