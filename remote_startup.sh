ed14841b3a266e04f42eb19db9ed873e93b66a95
quoc <quocnd@inter-k.com>
2023-08-18 05:03:53 +0700

    change the position of postgres in the remote_startup.sh

--- remote_startup.sh
+++ remote_startup.sh
@@ -2,6 +2,11 @@
 
 sudo snap install docker
 
+POSTGRES_CONTAINER_ID=$(sudo docker ps -qf "name=mentoree-db")
+if [ -z "$POSTGRES_CONTAINER_ID" ]; then
+  sudo docker run -dit --name mentoree-db -e POSTGRES_DB=mentoree -e POSTGRES_USER=ecomerce -e POSTGRES_PASSWORD=1234 -p 49153:5432 postgres:latest
+fi
+
 IMAGE=gcr.io/ecomercebackend-393408/cs428-backend:latest
 
 sudo gcloud auth print-access-token | sudo docker login -u oauth2accesstoken \
@@ -14,11 +19,6 @@ CONTAINER_ID=$(sudo docker ps -qf "name=backend")
 if [ ! -z "$CONTAINER_ID" ]; then
   sudo docker stop $CONTAINER_ID
   sudo docker rm -v $CONTAINER_ID
-fi
-
-POSTGRES_CONTAINER_ID=$(sudo docker ps -qf "name=mentoree-db")
-if [ -z "$POSTGRES_CONTAINER_ID" ]; then
-  sudo docker run -dit --name mentoree-db -e POSTGRES_DB=mentoree -e POSTGRES_USER=ecomerce -e POSTGRES_PASSWORD=1234 -p 49153:5432 postgres:latest
 fi
 
 sudo docker run -d -p 8000:8000 --name backend $IMAGE
