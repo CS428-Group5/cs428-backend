23bd723b8629be8074d2e47923055e90c18c7660
quoc <quocnd@inter-k.com>
2023-08-18 20:05:06 +0700

    fix the docker file

--- remote_startup.sh
+++ remote_startup.sh
@@ -2,9 +2,13 @@
 
 sudo snap install docker
 
-POSTGRES_CONTAINER_ID=$(sudo docker ps -qf "name=mentoree-db")
-if [ -z "$POSTGRES_CONTAINER_ID" ]; then
+POSTGRES_CONTAINER_NAME="mentoree-db"
+if ! (sudo docker ps | grep -q "$POSTGRES_CONTAINER_NAME") && ! (sudo docker ps -a | grep -q "$POSTGRES_CONTAINER_NAME"); then
   sudo docker run -dit --name mentoree-db -e POSTGRES_DB=mentoree -e POSTGRES_USER=ecomerce -e POSTGRES_PASSWORD=1234 -p 49153:5432 postgres:latest
+  echo "Do not have the database ... going to create a new one"
+elif ! (sudo docker ps | grep -q "$POSTGRES_CONTAINER_NAME"); then
+  echo "Database is running in the background..."
+  sudo docker start "$POSTGRES_CONTAINER_NAME"
 fi
 
 IMAGE=gcr.io/ecomercebackend-393408/cs428-backend:latest
@@ -15,10 +19,15 @@ sudo gcloud auth print-access-token | sudo docker login -u oauth2accesstoken \
 sudo gcloud auth configure-docker https://gcr.io
 sudo docker pull $IMAGE
 
-CONTAINER_ID=$(sudo docker ps -qf "name=backend")
-if [ ! -z "$CONTAINER_ID" ]; then
-  sudo docker stop $CONTAINER_ID
-  sudo docker rm -v $CONTAINER_ID
+WEBSERVER_CONTAINER="backend"
+if sudo docker ps | grep -q "$WEBSERVER_CONTAINER"; then
+  echo "The container is running in the foreground"
+  sudo docker stop "$WEBSERVER_CONTAINER"
+fi
+
+if sudo docker ps -a | grep -q "$WEBSERVER_CONTAINER"; then
+  echo "The container is running in the background"
+  sudo docker rm -v "$WEBSERVER_CONTAINER"
 fi
 
 sudo docker run -d -p 8000:8000 --name backend $IMAGE
